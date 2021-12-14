219e0e4b24ab16bb569372cfdcd1884bc771bdc5
Abedalhkeem Najeeb <abod.maas@gmail.com>
2021-12-14 14:15:35 +0100

    implemented linear regression from scratch for multivariables

--- linear regression/linear_regression.py
+++ linear regression/linear_regression.py
@@ -1,37 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plot
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 def loss_function(x, y, slopes, intercept):
-
     """ Calculate loss function (error = (y_hat - y)^2 / n). 
-    
     Args:
         x (pandas data frame of float): data variables
         y (pandas data frame of float): class label
         slopes (numpy list of floats): slope of the line
         intercept (float): intercept of line
     Return:
-        m (numpy list of floats): slope of the line  (theta)
-        b (float): intercept
+        total_error (float): loss error
     """
-
     total_error = 0
     for i in range(len(y)):
         if len(slopes) > 1:
              mult = slopes.dot(x.iloc[i].values)    
         else:
-            mult = x.iloc[i]* slopes[0]
-
-        total_error += ((mult+intercept) - y.iloc[1])**2
-
-        
-        
+            mult = x.iloc[i].values[0]* slopes[0]
+        total_error += ((mult+intercept) - y.iloc[i].values[0])**2
     return total_error/len(y)
 
 def gradient_descent(x,y,slopes, intercept, L):
     """ Calculate slopes and intercepts using gradient descent. 
-    
     Args:
         x (pandas data frame of float): data variables
         y (pandas data frame of float): class label
@@ -52,17 +44,15 @@ def gradient_descent(x,y,slopes, intercept, L):
             # More than 1D data
             mult = slopes.dot(x.iloc[i].values)
             for j in range(num_features):   
-                gradient_m[j] = gradient_m[j] + (-(2/n) * x.iloc[i].values[j] * (y.iloc[i] - (mult + intercept)))
+                gradient_m[j] = gradient_m[j] + (-(2/n) * x.iloc[i].values[j] * (y.iloc[i].values[0] - (mult + intercept)))
         else:
             # 1D data
-            mult = x.iloc[i]* slopes[0]
-            gradient_m[0] = gradient_m[0] + (-(2/n) * x.iloc[i] * (y.iloc[i] - (mult + intercept)))
+            mult = x.iloc[i].values[0]* slopes[0]
+            gradient_m[0] = gradient_m[0] + (-(2/n) * x.iloc[i].values[0] * (y.iloc[i].values[0] - (mult + intercept)))
             
-        gradient_b += (-(2/n) * (y.iloc[i] - (mult + intercept)))
-    
+        gradient_b += (-(2/n) * (y.iloc[i].values[0] - (mult + intercept)))
     m = np.zeros(num_features) 
     for i in range(num_features):
         m[i] = slopes[i] - (gradient_m[i] * L)
-
     b = intercept - gradient_b * L
-    return np.nan_to_num(m), np.nan_to_num(b)
+    return m, b
